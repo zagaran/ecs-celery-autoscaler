9f9a6c0ac2609f8d7702409f7c1567e767019847
theoyannekis <33228851+theoyannekis@users.noreply.github.com>
2026-08-18 10:02:19 -0400

    export latency metric

--- ecs_celery_autoscaler/ecs_celery_autoscaler/__init__.py
+++ ecs_celery_autoscaler/ecs_celery_autoscaler/__init__.py
@@ -1,3 +1,3 @@
 from .autoscaler import EcsCeleryAutoscaler, QueueDepthMetric, ScalingMetric
 
-__all__ = ["EcsCeleryAutoscaler", "QueueDepthMetric", "ScalingMetric"]
+__all__ = ["EcsCeleryAutoscaler", "QueueDepthMetric", "QueueLatencyMetric", "ScalingMetric"]
